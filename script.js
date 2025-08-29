cbf21d692005261181c339db3ad8a8dc8d38e7da
Kimia Rezaei <82454144+kimiarezaei@users.noreply.github.com>
2025-08-29 18:30:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// function sayHello() {
+//     alert("Hello from an external JavaScript file!");
+// }
+
+function sayHello() {
+    document.getElementById('output').innerText = "Hi, I'm Kimia. Welcome! You can contact me via LinkedIn for questions or collaboration opportunities.";
+}
