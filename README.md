61ff431e39fe4f64e1a59dd3830165f17847d8aa
Kimia Rezaei <82454144+kimiarezaei@users.noreply.github.com>
2025-10-01 13:32:47 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Personal Weblog
+
+This is my personal weblog built using **HTML, CSS, and a little JavaScript**.  
+It has three main sections:
+
+- **Home** – Landing page with a welcome message.
+- **About Me** – A short introduction about myself.
+- **CV** – My curriculum vitae (resume).  
+  
---
+++ script.js
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+// function sayHello() {
+//     alert("Hello from an external JavaScript file!");
+// }
+
+function sayHello() {
+    document.getElementById('output').innerText = "Hi, I'm Kimia. Welcome! You can contact me via LinkedIn for questions or collaboration opportunities.";
+}
+=======
+// function sayHello() {
+//     alert("Hello from an external JavaScript file!");
+// }
+
+function sayHello() {
+    document.getElementById('output').innerText = "Hi, I'm Kimia. Welcome! You can contact me via LinkedIn for questions or collaboration opportunities.";
+}
+>>>>>>> 7408060 (initate commit)
