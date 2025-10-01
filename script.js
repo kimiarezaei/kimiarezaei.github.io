bc5a394e2422374cec15b2ba61d9bcdeb0f80767
Kimia Rezaei <82454144+kimiarezaei@users.noreply.github.com>
2025-10-01 13:27:37 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// function sayHello() {
-//     alert("Hello from an external JavaScript file!");
-// }
-
-function sayHello() {
-    document.getElementById('output').innerText = "Hi, I'm Kimia. Welcome! You can contact me via LinkedIn for questions or collaboration opportunities.";
-}
